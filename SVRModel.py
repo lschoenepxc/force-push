e6cabac5a21bae7c7b1b8472c3ca448a27531b02
LauraSchöne <schoene_l@web.de>
2024-06-09 09:47:24 +0200

    Added full SVR Model

--- SVRModel.py
+++ SVRModel.py
@@ -37,7 +37,7 @@ def read_csv_file(file_path):
 # Beispiel: Daten aus "input.csv" einlesen
 
 
-def getSVRPrediction(inputData):
+def getSVRPrediction(inputData, outputPara):
     data = pd.read_csv("initial_data.csv")
     cleaned_data = read_csv_file("querys_ForcePush.csv")
     df_queried = pd.DataFrame(cleaned_data[1:], columns=cleaned_data[0])
@@ -59,67 +59,67 @@ def getSVRPrediction(inputData):
 
     # Teile die Daten in Features (X) und Ziel (y) auf
     X = data[["Engine speed", "Engine load", "Railpressure", "Air supply", "Crank angle", "Intake pressure", "Back pressure", "Intake temperature"]].values
-    y = data[["NOx", "PM 1", "CO2", "Pressure cylinder"]].values
-
-    # Generiere Beispieldaten
-    #X_sample, y_sample = make_regression(n_samples=100, n_features=8, noise=0.2, random_state=42)
+    # y = data[["NOx", "PM 1", "CO2", "Pressure cylinder"]].values
+    if outputPara == 0:
+        y = data[["NOx"]].values
+    elif outputPara == 1:
+        y = data[["PM 1"]].values
+    elif outputPara == 2:
+        y = data[["CO2"]].values
+    elif outputPara == 3:
+        y = data[["Pressure cylinder"]].values
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Standardisiere die Features
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X_train)
-    # X_scaled = scaler.fit_transform(X_sample)
 
-    # Trainiere ein Support Vector Regressor (SVR)-Modell
-    svr = SVR(kernel="rbf", C=200, epsilon=0.01) #beste werte waren für mich kernel="rbf" c=200 und epsilon=0.001
-    # svr = SVR(kernel='rbf', C=20.0, epsilon=0.1) # Cross-Validation:
-    # svr = SVR()
+    # Grid-Search für Hyperparameter
+    svr = SVR()
 
-    # # Wende Kreuzvalidierung an
-    # scores = cross_val_score(svr, X_scaled, y_sample, cv=5, scoring='neg_mean_squared_error')
-    # # Berechne den Durchschnitt und die Standardabweichung der Scores
-    # mean_score = scores.mean()
-    # std_dev = scores.std()
-
-    # print(f"Durchschnittlicher Score: {mean_score}")
-    # print(f"Standardabweichung der Scores: {std_dev}")
+    # Definiere das Parametergitter
+    param_grid = {
+        'estimator__C': [0.1, 1, 10, 100, 200],
+        'estimator__epsilon': [0.01, 0.1, 1, 10],
+        'estimator__kernel': ['linear', 'poly', 'rbf', 'sigmoid']
+    }
 
     multi_output_svr = MultiOutputRegressor(svr)
+    # Initialisiere die Grid-Suche
+    grid_search = GridSearchCV(multi_output_svr, param_grid, cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
 
-    # # Definiere das Parametergitter
-    # param_grid = {
-    #     'estimator__C': [0.1, 1, 10, 100],  # Beispielwerte
-    #     'estimator__epsilon': [0.01, 0.1, 1, 10],
-    #     'estimator__kernel': ['linear', 'poly', 'rbf', 'sigmoid']
-    # }
-    # # Initialisiere die Grid-Suche
-    # grid_search = GridSearchCV(multi_output_svr, param_grid, cv=5, scoring='neg_mean_squared_error', verbose=2, n_jobs=-1)
+    # Passe die Grid-Suche an die Daten an
+    grid_search.fit(X_scaled, y_train)
 
-    # # Passe die Grid-Suche an die Daten an
-    # grid_search.fit(X_scaled, y_train)
+    # Ausgabe der besten Parameter und des besten Scores
+    print("Beste Parameter: ", outputPara, grid_search.best_params_) # Beste Parameter: {'estimator__C': 200, 'estimator__epsilon': 1, 'estimator__kernel': 'rbf'}
+    print("Bester Score:",outputPara,grid_search.best_score_)
 
-    # # Ausgabe der besten Parameter und des besten Scores
-    # print("Beste Parameter:", grid_search.best_params_) # Beste Parameter: {'estimator__C': 200, 'estimator__epsilon': 1, 'estimator__kernel': 'rbf'}
-    # print("Bester Score:", grid_search.best_score_)
+    # Trainiere ein Support Vector Regressor (SVR)-Modell
+    # SVR with best_params_
+    svr = SVR(kernel=grid_search.best_params_['estimator__kernel'], C=grid_search.best_params_['estimator__C'], epsilon=grid_search.best_params_['estimator__epsilon'])
+    #svr = SVR(kernel=hyperParas[0], C=hyperParas[1], epsilon=hyperParas[2]) #beste werte waren für mich kernel="rbf" c=200 und epsilon=0.001
+    
+    multi_output_svr = MultiOutputRegressor(svr)
     
     multi_output_svr.fit(X_scaled, y_train)
 
     X_test_scaled = scaler.transform(X_test)
     y_pred = multi_output_svr.predict(X_test_scaled)
-    # y_pred = grid_search.predict(X_test_scaled)
-
-    accuracy = []
-
-    for i in range(0,4):
-        #mape = sklearn.mean(np.abs((y_test[:,i] - y_pred[:,i])/y_test[:,i])) * 100
-        mape= mean_absolute_percentage_error(y_test[:,i], y_pred[:,i]) * 100
-        accuracy.append(100-mape)
-        #print("accuracy = " + str(100-mape))
-
+    accuracy=100 - mean_absolute_percentage_error(y_test, y_pred) * 100
 
     # Mache Vorhersagen auf dem Testset
     inputData_Scaled = scaler.transform(inputData)
     y_pred = multi_output_svr.predict(inputData_Scaled)
-    # y_pred = grid_search.predict(inputData_Scaled)
+
+    # Wende Kreuzvalidierung an
+    scores = cross_val_score(svr, X_scaled, y_train, cv=5, scoring='neg_mean_squared_error', verbose=0)
+    # Berechne den Durchschnitt und die Standardabweichung der Scores
+    mean_score = scores.mean()
+    std_dev = scores.std()
+
+    print(f"Durchschnittlicher Score: {mean_score}")
+    print(f"Standardabweichung der Scores: {std_dev}")
+
     return accuracy, y_pred
